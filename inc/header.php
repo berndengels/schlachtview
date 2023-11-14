e5c5d31f137bf7f09428faffb5f491af5f3d997f
bengels <engels@f50.de>
2023-11-14 22:31:59 +0100

    first commit

---
+++ inc/header.php
@@ -0,0 +1,12 @@
+<?php require_once 'inc/Media.php'; ?>
+<html>
+<head>
+	<meta title="Schlachtvieh - Schocktherapie">
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="/bootstrap/css/bootstrap.css" />
+	<link rel="stylesheet" href="/css/app.css" />
+	<script src="/js/jquery-3.7.1.min.js"></script>
+	<script src="/bootstrap/js/bootstrap.js"></script>
+	<script src="/js/app.js"></script>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'inc/header.php';
+$files = \inc\Media::getMedia();
+?>
+<div class="container-fluid">
+	<div class="row align-content-center justify-content-center">
+		<div class="col-sm-12 col-lg-4 align-self-center d-inline-block align-middle">
+			<h5 class="mt-5">Schlachtview - Schocktherapie</h5>
+			<table class="media">
+				<?php foreach ($files as $file): ?>
+					<tr>
+						<td><?php echo $file['name']; ?></td>
+						<td>
+							<audio controls preload="none" style="width:280px;">
+								<source src="<?php echo $file['path']; ?>" type="audio/mp4" />
+								<p>Your browser does not support HTML5 audio.</p>
+							</audio>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+			</table>
+		</div>
+	</div>
+</div>
+<?php require_once 'inc/footer.php' ?>
